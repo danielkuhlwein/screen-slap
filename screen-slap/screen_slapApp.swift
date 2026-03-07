9a64a76da382a2c2ea79ed03ace071ee44faee3f
danielkuhlwein <danielkuhlwein@me.com>
2026-03-07 10:47:32 -0700

    Initial Commit

---
+++ screen-slap/screen_slapApp.swift
@@ -0,0 +1,17 @@
+//
+//  screen_slapApp.swift
+//  screen-slap
+//
+//  Created by Daniel Kuhlwein on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct screen_slapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
